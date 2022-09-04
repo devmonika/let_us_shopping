e177885ec6d1100081b024aeb92b56cbf8a990cd
devmonika <devmonikamomi@gmail.com>
2022-09-04 20:27:17 +0600

    first commit

---
+++ shop.js
@@ -0,0 +1,105 @@
+//get categories
+
+const getCatUrl = async () => {
+    const url = `https://fakestoreapi.com/products/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayProCat(data);
+    console.log(data);
+  }
+  //display cat
+  const displayProCat = ctaegories => {
+  
+    const catContainer = document.getElementById('category-container');
+    ctaegories.forEach(category => {
+    //   console.log(typeof category);
+    // console.log(category);
+      const li = document.createElement('li');
+      li.innerHTML = `
+          <span onclick="loadAllNews('${category_id}')" class="text-2xl capitalize">${category}</span>
+          `;
+    catContainer.appendChild(li);
+  
+    });
+  
+  }
+  //getcat id
+//   const getCatId = async (category_id) => {
+//     const url = `https://fakestoreapi.com/products/categories/${category_id}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displayProCat(data);
+//     console.log(data);
+//   }
+
+
+
+
+
+
+
+// get all product 
+
+const getShopApi = async() =>{
+    const url=`https://fakestoreapi.com/products`;
+    const res =await fetch(url);
+    const data = await res.json();
+    showShopData(data);
+
+}
+
+// show all product 
+
+const showShopData = shopDatas => {
+    const ShopItem = document.getElementById('show-item');
+    shopDatas = shopDatas.slice(0,9);
+    shopDatas.forEach(shopData  => {
+    // console.log(shopData);
+    const showItemDiv = document.createElement('div');
+    showItemDiv.classList.add('card', 'w-96', 'bg-base-100', 'shadow-xl');
+    showItemDiv.innerHTML =`
+        <figure class="px-10 pt-10">
+          <img src="${shopData.image ? shopData.image : 'no img found'}" alt="Shoes" class="rounded-xl h-60" />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">${shopData.title ? shopData.title : 'no title found'}</h2>
+          <p>${shopData.description.slice(0,105)}...</p>
+          <div class="card-actions">
+            <label onclick="productPOpUp('${shopData.id}')" for="my-modal-6" class="btn modal-button w-40 hover:bg-violet-900">Shop</label>
+          </div>
+        </div>
+    `;
+    ShopItem.appendChild(showItemDiv);
+
+});
+}
+
+// product id 
+const productPOpUp = async(id) =>{
+    const url =`https://fakestoreapi.com/products/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data);
+    showProductDetails(data);
+}
+
+// show modal details 
+const showProductDetails = (shopData) =>{
+    console.log(shopData);
+    const showDetailsOfProduct = document.getElementById('show-details');
+    showDetailsOfProduct.textContent='';
+    showDetailsOfProduct.innerHTML=`
+    <div class="modal-box">
+        <img src="${shopData.image ? shopData.image : 'no img found'}" class="h-5/6"/>
+        <h3 class="font-bold text-lg">${shopData.title ? shopData.title : 'no data dound'}</h3>
+        <p class="py-4">${shopData.description ? shopData.description : 'no data found'}</p>
+        <div class="modal-action">
+        <label for="my-modal-6" class="btn">Close</label>
+        </div>
+    </div>
+    `;
+};
+
+getCatUrl();
+// productPOpUp();
+getShopApi();
